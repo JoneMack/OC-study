1a9f32420a046a61b7c744e683f4dec9b325c8b1
xubojoy <xubojoy1112@163.com>
2017-08-10 11:02:05 +0800

    添加步数读写

---
+++ 步数读写/步数读写/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  步数读写
+//
+//  Created by 张帅 on 2017/4/7.
+//  Copyright © 2017年 张帅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 步数读写/步数读写/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  步数读写
+//
+//  Created by 张帅 on 2017/4/7.
+//  Copyright © 2017年 张帅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
